e42297e7dd4f9dfc3639e98112a819411e278d9e
PooiaFerdowsi <76428946+PooiaFerdowsi@users.noreply.github.com>
2021-03-01 16:00:53 +0330

    Update main.c

--- main.c
+++ main.c
@@ -1,1 +1,14 @@
 #include <stdio.h>
+
+int main () {
+  int num; // number user input
+  scanf("enter The Number %d",&num); // get correct value of $number of user
+  if(num%2==0&num<4){
+    int loop=0; // Loop number 
+    while(loop<num){
+      printf("I");
+      loop+=1;
+    }
+  }
+  return 0; // status code
+}
